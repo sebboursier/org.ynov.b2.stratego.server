1e087d5a5fa194777fdfb487305f150b8cd63940
Sébastien Boursier <seb.boursier.33@gmail.com>
2018-09-06 17:54:59 +0200

    all repos

--- src/main/java/org/ynov/b2/stratego/server/repository/TeamRepository.java
+++ src/main/java/org/ynov/b2/stratego/server/repository/TeamRepository.java
@@ -5,13 +5,13 @@ package org.ynov.b2.stratego.server.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.ynov.b2.stratego.server.model.Player;
+import org.ynov.b2.stratego.server.model.Team;
 
 /**
  * @author sebboursier
  *
  */
 @RepositoryRestResource
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
+public interface TeamRepository extends JpaRepository<Team, Integer> {
 
 }
